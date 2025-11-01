647ae2a4280e5df550ad8e429734c983d82c3fb3
MODUGGAGI <kiroro0814@naver.com>
2025-11-01 17:35:01 +0900

    test(service): LottoService 테스트 추가
    - validator 추가로 인한 코드 이동
    - 당첨 결과 계산 테스트 추가
    - 수익률 계산 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,51 +1,30 @@
 package lotto.service;
 
-import lotto.Lotto;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoServiceTest {
 
     private final LottoService lottoService = new LottoService();
 
+    @DisplayName("구입 금액으로 로또 개수 확인하기")
     @Test
-    @DisplayName("숫자가 아닌 문자가 섞여 들어왔을 경우")
-    void 숫자X_입력값() {
-        assertThatThrownBy(() -> lottoService.validateMoney("1000j"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자를 입력해 주세요.");
+    void 구입금액_로또개수_확인() {
+        int quantity = lottoService.calculateLottoQuantity(5000);
+
+        assertThat(quantity).isEqualTo(5);
     }
 
-    @Test
-    @DisplayName("1,000원으로 나누어떨어지지 않는 숫자가 들어온 경우")
-    void 나누어_떨어지지_X_숫자_입력() {
-        assertThatThrownBy(() -> lottoService.validateMoney("1500"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 로또 구입 금액은 1,000원으로 나누어떨어져야 합니다.");
-    }
-
-    @Test
-    @DisplayName("랜덤으로 생성된 로또 번호가 정렬")
-    void 로또_번호_정렬() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    List<Lotto> lottoList = lottoService.generateLottos(1);
-                    Lotto lotto = lottoList.getFirst();
-
-                    assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
-                },
-                List.of(5, 3, 1, 6, 2, 4)
-        );
-    }
-
-    @Test
     @DisplayName("주어진 개수 만큼 로또 생성")
+    @Test
     void 로또_생성() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
@@ -57,6 +36,43 @@ class LottoServiceTest {
                 List.of(7, 8, 9, 10, 11, 12),
                 List.of(13, 14, 15, 16, 17, 18)
         );
+    }
+
+    @DisplayName("당첨 결과 계산 테스트")
+    @Test
+    void 당첨_결과_테스트() {
+        // given
+        List<Lotto> lottoList = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7))
+        );
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        // when
+        Map<Rank, Integer> result = lottoService.calculateResult(lottoList, winningLotto, bonusNumber);
+
+        // then
+        assertThat(result.get(Rank.FIRST)).isEqualTo(1);
+        assertThat(result.get(Rank.SECOND)).isEqualTo(1);
+        assertThat(result.get(Rank.THIRD)).isEqualTo(0);
+    }
+
+    @DisplayName("수익률 계산 테스트")
+    @Test
+    void 수익률_계산_테스트() {
+        // given
+        List<Lotto> lottoList = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6))
+        );
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+
+        // when
+        Map<Rank, Integer> result = lottoService.calculateResult(lottoList, winningLotto, 7);
+        double yields = lottoService.calculateYields(1000, result);
+
+        // then
+        assertThat(yields).isEqualTo(500.0);
     }
 
 }
