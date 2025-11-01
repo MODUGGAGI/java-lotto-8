7e026d268e70a8a2c65f15ca9420fc45ab8134ac
MODUGGAGI <kiroro0814@naver.com>
2025-11-01 17:30:06 +0900

    chore(Application): 완료한 TODO 주석 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         LottoController controller = new LottoController();
         controller.startLotto();
-        // TODO: 프로그램 구현
     }
 }
