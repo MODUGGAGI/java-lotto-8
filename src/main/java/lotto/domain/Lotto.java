bf1674731db61755c2c31f12506b727080e04f9a
MODUGGAGI <kiroro0814@naver.com>
2025-11-01 17:27:50 +0900

    feat(Lotto): 로또 번호 자동 정렬 및 검증 로직 추가
    - 로또 생성 시 번호 자동 정렬, 중복 검사를 추가
    - 다른 로또와의 일치하는 번호 개수를 반환하는 메소드를 추가
    - 보너스 번호에 대해서 중복 검사, 포함 여부를 검사하는 메소드를 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.message.ErrorMessage;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = sortNumbers(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        int originalSize = numbers.size();
+        if (originalSize != 6) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_NUMBER.toString());
+        }
+
+        int uniqueCount = (int) numbers.stream().distinct().count();
+        if (originalSize != uniqueCount) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.toString());
+        }
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    public int countMatchingNumbers(Lotto other) {
+        return (int) this.numbers.stream()
+                .filter(other.numbers::contains)
+                .count();
+    }
+
+    public boolean containsNumber(int bonusNumber) {
+        return this.numbers.contains(bonusNumber);
+    }
+
+    public void validateBonusNumber(int bonusNumber) {
+        if (this.numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBER.toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+}
