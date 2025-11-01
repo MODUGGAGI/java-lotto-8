602babd47e3cfdd697d77f0d14aa742d625f8da2
MODUGGAGI <kiroro0814@naver.com>
2025-11-01 17:28:48 +0900

    feat(controller): 당첨 결과 계산 및 출력 기능 추가
    - 당첨 번호 및 보너스 번호 입력 처리
    - 당첨 통계 및 수익률 출력
    - 예외 발생 시 재입력 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,11 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.Lotto;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
 import lotto.service.LottoService;
+import lotto.validator.LottoValidator;
 import lotto.view.LottoView;
 
 public class LottoController {
@@ -15,12 +18,43 @@ public class LottoController {
         int quantity = service.calculateLottoQuantity(money);
         List<Lotto> lottoList = service.generateLottos(quantity);
         view.printLottoNumbers(quantity, lottoList);
+
+        Lotto winningLotto = getWinningLotto();
+        int bonusNumber = getBonusNumber(winningLotto);
+        Map<Rank, Integer> result = service.calculateResult(lottoList, winningLotto, bonusNumber);
+        double yields = service.calculateYields(money, result);
+
+        view.printResult(result);
+        view.printYields(yields);
+    }
+
+    private int getBonusNumber(Lotto winningLotto) {
+        try {
+            String rawNumber = view.getBonusNumber();
+            int bonusNumber = LottoValidator.parseBonusNumber(rawNumber);
+            winningLotto.validateBonusNumber(bonusNumber);
+            return bonusNumber;
+        } catch (IllegalArgumentException e) {
+            view.printError(e.getMessage());
+            return getBonusNumber(winningLotto);
+        }
+    }
+
+    private Lotto getWinningLotto() {
+        try {
+            String rawNumbers = view.getWinningLotto();
+            List<Integer> numbers = LottoValidator.parseWinningNumbers(rawNumbers);
+            return service.generateWinningLotto(numbers);
+        } catch (IllegalArgumentException e) {
+            view.printError(e.getMessage());
+            return getWinningLotto();
+        }
     }
 
     private int getMoney() {
         try {
             String rawMoney = view.getMoney();
-            return service.validateMoney(rawMoney);
+            return LottoValidator.parseMoney(rawMoney);
         } catch (IllegalArgumentException e) {
             view.printError(e.getMessage());
             return getMoney();
