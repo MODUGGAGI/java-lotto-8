6a8d8e4e7826c3fc74d577465f12f76f269e4426
MODUGGAGI <kiroro0814@naver.com>
2025-11-01 17:28:55 +0900

    feat(view): 당첨 번호 입력, 당첨 통계 출력 추가
    - 당첨 번호와 보너스 번호 입력, 당첨 통계 출력을 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -2,27 +2,50 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
-import lotto.InputMessage;
-import lotto.Lotto;
-import lotto.OutputMessage;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.Rank;
+import lotto.message.InputMessage;
+import lotto.message.OutputMessage;
 
 public class LottoView {
 
     public String getMoney() {
-        System.out.println(InputMessage.GET_MONEY.message);
+        System.out.println(InputMessage.GET_MONEY);
+        return Console.readLine();
+    }
+
+    public String getWinningLotto() {
+        System.out.println(InputMessage.GET_WINNING_LOTTO);
+        return Console.readLine();
+    }
+
+    public String getBonusNumber() {
+        System.out.println(InputMessage.GET_BONUS_NUMBER);
         return Console.readLine();
     }
 
     public void printLottoNumbers(int quantity, List<Lotto> lottoList) {
         System.out.println();
-        System.out.printf((OutputMessage.LOTTO_QUANTITY.message) + "%n", quantity);
+        System.out.printf(OutputMessage.LOTTO_QUANTITY + "\n", quantity);
 
         for (Lotto lotto : lottoList) {
             System.out.println(lotto.toString());
         }
     }
 
+    public void printResult(Map<Rank, Integer> result) {
+        System.out.println(OutputMessage.LOTTO_STATISTICS);
+        for (Rank rank : result.keySet()) {
+            System.out.printf(rank.toString() + "\n", result.get(rank));
+        }
+    }
+
+    public void printYields(double yields) {
+        System.out.printf(OutputMessage.LOTTO_YIELDS + "\n", yields);
+    }
+
     public void printError(String errorMessage) {
-        System.out.println("\n" + errorMessage + "\n");
+        System.out.println("\n" + errorMessage);
     }
 }
