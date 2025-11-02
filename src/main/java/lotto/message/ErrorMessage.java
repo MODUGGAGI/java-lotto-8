22d6073d363bd061f09e55c204ef28a83d4994ec
MODUGGAGI <kiroro0814@naver.com>
2025-11-03 01:28:11 +0900

    feat(message): 로또 번호 범위 검증 추가
    - 기존의 ENUM 이름 정리
    - 값 범위 검증 에러 추가

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -4,8 +4,9 @@ public enum ErrorMessage {
     INVALID_INPUT_FORMAT("[ERROR] 숫자를 입력해 주세요."),
     INVALID_MONEY("[ERROR] 로또 구입 금액은 1,000원으로 나누어떨어져야 합니다."),
     DUPLICATE_NUMBER("[ERROR] 당첨 번호는 중복될 수 없습니다."),
-    OUT_OF_RANGE_NUMBER("[ERROR] 당첨 번호는 6개여야 합니다."),
-    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    NUMBER_OUT_OF_SIZE("[ERROR] 당첨 번호는 6개여야 합니다."),
+    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
+    NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45까지의 숫자만 가능합니다.");
 
     private final String message;
 
